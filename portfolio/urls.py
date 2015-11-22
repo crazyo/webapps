7c27e9591974f23f54d0c35e4f33aca4d64afb72
wangx173 <wangx173@WangXiangs-MBP.leoguest.fg.rbc.com>
2015-11-21 22:50:16 -0500

    fixed comparasion not found

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^comparasion/', views.comparasion, name="comparasion"),
+
     url(r'^$', views.portfolio, name="portfolio"),
     url(r'^secondary_chart/', views.get_sub_chart, name="secondary_chart"),
+    url(r'^comparasion/', views.comparasion, name="comparasion"),
 ]
