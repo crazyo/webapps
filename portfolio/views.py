20f1cd21fc4d247fd5eea8c59ce967d6e5f24bb7
wangx173 <wangx173@WangXiangs-MBP.leoguest.fg.rbc.com>
2015-11-21 21:22:05 -0500

    added ana chart

--- portfolio/views.py
+++ portfolio/views.py
@@ -36,6 +36,69 @@ def comparasion(request):
 
 
 
+def return_rate_by_date(request):
+      #get all referring sites
+    ReferringSites  = ReturnRate.objects.all()
+    column = []
+    #loop through referring sites
+
+    for element in ReferringSites:
+        column.append([element.url.encode("utf-8"), Article.objects.filter(url = element.url).count(), element.name.encode("utf-8")])
+
+    #sort the referring sites by the number of articles they have.
+    column.sort(key = lambda x: x[1], reverse=True)
+    #get top 10 referring sites based on the number of articles
+    column = column[0:10]
+    msites_name = []
+    for a in range(len(column)):
+        msites_name.append(column[a][2])
+
+    for a in range(len(column)):
+        column[a] = column[a][0]
+
+    data = []
+
+    pre_date = None
+
+    #loop through articles
+    for art in Article.objects.all():
+        new=[]
+        #get day month, and year of each article
+        date = art.date_added.strftime("%B %d, %Y")
+        day = art.date_added.strftime("%d")
+        month = art.date_added.strftime("%m")
+        year = art.date_added.strftime("%Y")
+
+        if date != pre_date:
+            pre_date = date
+            new.append(date)
+            for url in column:
+                #count the number of articles added on that day, and add to the list.
+                new.append(Article.objects.filter(domain = url, date_added__day = day, date_added__month = month, date_added__year = year).count())
+            data.append(new)
+
+    return msites_name, data
+
+
+
+
+
+    return render(request, 'portfolio/_chart.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def get_sub_chart(category):
     dict =[]
     title = [category, 'Revenue']
